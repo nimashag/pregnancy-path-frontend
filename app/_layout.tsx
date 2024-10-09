78432350dc43c57c02807182525d6cd302447e0c
Rusiru Kalendra De Silva <IT22927316@my.sliit.lk>
2024-10-09 15:14:48 +0530

    added buttons and some minor updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="/index" options={{ headerShown: false }} />
         <Stack.Screen name="auth/login" options={{ headerShown: false }} />
