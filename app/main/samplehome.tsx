b9a35836a15d766cbb478129db365babb19d8330
Rusiru Kalendra De Silva <IT22927316@my.sliit.lk>
2024-10-11 11:37:32 +0530

    Update samplehome.tsx

--- app/main/samplehome.tsx
+++ app/main/samplehome.tsx
@@ -282,6 +282,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   header: {
+    marginTop: 15,
     flexDirection: 'row',
     justifyContent: 'space-between',  
     alignItems: 'center', 
